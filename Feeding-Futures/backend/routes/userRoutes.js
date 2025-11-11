d0a6bdc107fdb2e60dec390bfbe8b5c3b5000f47
Rishit Tandon <rishit.tandon.7@gmail.com>
2025-11-11 15:29:01 +0530

    update

--- Feeding-Futures/backend/routes/userRoutes.js
+++ Feeding-Futures/backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
-import User from "../models/user.js";  // Make sure this is your actual Mongoose model
+import User from "../models/User.js";  // Make sure this is your actual Mongoose model
 
 const router = express.Router();
 
